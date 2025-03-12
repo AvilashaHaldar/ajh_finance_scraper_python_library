b03fb76b7e8b4679c35e97e3a15487a51dd724da
AvilashaHaldar <jiniahaldarjh@gmail.com>
2025-03-12 02:23:59 -0400

    Removing unneeded files

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     packages=find_packages(include=['financeonlinedatascraper']),
     version='0.1.0',
     description='Library for scraping financial data from online sources',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='AJH',
     install_requires=['pandas', 'numpy', 'requests', 'lxml', 'yfinance'],
     setup_requires=['pytest-runner'],
