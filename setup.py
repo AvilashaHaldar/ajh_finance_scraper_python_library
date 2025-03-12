9e6d8fa8fc630af07ca1c128ee891e5629791b09
AvilashaHaldar <jiniahaldarjh@gmail.com>
2025-03-12 03:48:19 -0400

    Renaming module

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='financeonlinedatascraper',
-    packages=find_packages(include=['financeonlinedatascraper']),
+    name='ajh_finance_online_data_scraper',
+    packages=find_packages(include=['ajh_finance_online_data_scraper']),
     version='0.1.0',
     description='Library for scraping financial data from online sources',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author='AJH',
+    author='Avilasha Jinia Haldar',
     install_requires=['pandas', 'numpy', 'requests', 'lxml', 'yfinance'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'unittest', 'pandas', 'numpy'],
