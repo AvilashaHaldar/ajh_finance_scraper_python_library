6588f7a227222a9842a856521346e405ff9d7f82
AvilashaHaldar <jiniahaldarjh@gmail.com>
2025-03-12 01:21:44 -0400

    Initial commit for the financial scraper library

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='financeonlinedatascraper',
+    packages=find_packages(include=['financeonlinedatascraper']),
+    version='0.1.0',
+    description='Library for scraping financial data from online sources',
+    author='AJH',
+    install_requires=['pandas', 'numpy', 'requests', 'lxml', 'yfinance'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'unittest', 'pandas', 'numpy'],
+    test_suite='tests',
+)
